005846ec8c3f38dac165e7738f47b0eff6c0c55d
Leonn190 <euleonsoto@gmail.com>
2026-03-10 09:25:29 -0300

    animaçao corrigida

--- Codigo/Modulos/Auxiliares.py
+++ Codigo/Modulos/Auxiliares.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import pygame
 
-
 def carregar_frames(pasta, loader=None):
-    arquivos = sorted(Path(pasta).glob("*.png"))
+    arquivos = sorted(
+        Path(pasta).glob("*.png"),
+        key=lambda arq: int(arq.stem.split("_")[-1])
+    )
 
     if loader is None:
         return [pygame.image.load(str(arquivo)).convert_alpha() for arquivo in arquivos]

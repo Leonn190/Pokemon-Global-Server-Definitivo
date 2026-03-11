9d3c7efd2da57bd3343bb664c75544e94556eac6
Leonn190 <euleonsoto@gmail.com>
2026-03-11 12:22:02 -0300

    bug das musicas

--- Codigo/Geradores/Player/Controle.py
+++ Codigo/Geradores/Player/Controle.py
@@ -204,9 +204,6 @@ class Controle:
         px, py = self.Ator.Posicao
         self.Ator.definir_posicao(px % largura, py % altura)
 
-    def bloco_atual(self):
-        return self._tile_atual()
-
     def _tile_atual(self):
         x, y = self.Ator.Posicao
         bx = int(math.floor(x))
